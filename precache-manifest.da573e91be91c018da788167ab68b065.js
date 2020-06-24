825362a96ca7b04d316c74c9ee19cddb6b95c540
mark-nikerin <mrnikerin@gmail.com>
2020-06-25 02:20:02 +0400

    Updates

---
+++ precache-manifest.da573e91be91c018da788167ab68b065.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a17465883c60d666f22000648e7bb9d",
+    "url": "/React-ToDo-App/index.html"
+  },
+  {
+    "revision": "504543023dbabfd71465",
+    "url": "/React-ToDo-App/static/css/main.3b415e74.chunk.css"
+  },
+  {
+    "revision": "fddffd3fb722a490a6e2",
+    "url": "/React-ToDo-App/static/js/2.f94ae0e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDo-App/static/js/2.f94ae0e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4aeab4641d071daf66d",
+    "url": "/React-ToDo-App/static/js/3.52e07cea.chunk.js"
+  },
+  {
+    "revision": "504543023dbabfd71465",
+    "url": "/React-ToDo-App/static/js/main.ae71f333.chunk.js"
+  },
+  {
+    "revision": "e8891741810b2b139d9d",
+    "url": "/React-ToDo-App/static/js/runtime-main.1caa7786.js"
+  }
+]);
